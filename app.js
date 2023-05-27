705edbf5e362e0c679bc3e615feca9bc327427a0
Wilkins Morales <wilkinscom@gmail.com>
2023-05-27 10:57:55 +0100

    change cors to *

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const cors = require("cors");
 
+/*
 // Configure CORS options
 const corsOptions = {
   origin: "*",
 };
+*/
 
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
@@ -17,9 +19,13 @@ const express = require("express");
 
 // Apply CORS middleware globally
 const app = express();
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use("/uploads", cors(corsOptions), express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config/index")(app);
@@ -29,26 +35,26 @@ const cognitoAuthMiddleware = cognitoAuth.getVerifyMiddleware();
 
 // 👇 Start handling routes here
 const courseRoutes = require("./routes/course.routes");
-app.use("/api", cognitoAuthMiddleware, cors(corsOptions), courseRoutes);
+app.use("/api", cognitoAuthMiddleware, courseRoutes);
 
 const reviewRoutes = require("./routes/review.routes");
 //console.log("Review routes imported:", reviewRoutes);
-app.use("/api", cognitoAuthMiddleware, cors(corsOptions), reviewRoutes);
+app.use("/api", cognitoAuthMiddleware, reviewRoutes);
 
 const lecturerRoutes = require("./routes/lecturer.routes");
-app.use("/api", cognitoAuthMiddleware, cors(corsOptions), lecturerRoutes);
+app.use("/api", cognitoAuthMiddleware, lecturerRoutes);
 
 const userRoutes = require("./routes/user.routes");
-app.use("/user", cognitoAuthMiddleware, cors(corsOptions), userRoutes);
+app.use("/user", cognitoAuthMiddleware, userRoutes);
 
 const authRoutes = require("./routes/auth.routes");
-app.use("/auth", cognitoAuthMiddleware, cors(corsOptions), authRoutes);
+app.use("/auth", cognitoAuthMiddleware, authRoutes);
 
 const chatperRoutes = require("./routes/chapter.routes");
-app.use("/api", cognitoAuthMiddleware, cors(corsOptions), chatperRoutes);
+app.use("/api", cognitoAuthMiddleware, chatperRoutes);
 
 const indexRoutes = require("./routes/index.routes");
-app.use("/api", cognitoAuthMiddleware, cors(corsOptions), indexRoutes);
+app.use("/api", cognitoAuthMiddleware, indexRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
